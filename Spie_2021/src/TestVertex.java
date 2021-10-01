cf16953d98b1e91fb2956adb497de8f08222e402
servver <servver@servver-ESPRIMO-Mobile-M9410>
2021-10-01 09:09:46 +0200

    new features

--- Spie_2021/src/TestVertex.java
+++ Spie_2021/src/TestVertex.java
@@ -2,19 +2,33 @@
 public class TestVertex {
 
 	public static void main(String[] args) {
+			
+		
 		
 		Vertex v1=new Vertex(17, 4);
-		Vertex maus=new Vertex(24, 0);
-		Vertex v3=v1.skalarMult(42.2);
-		Vertex katze=new Vertex(1,1);
-		Vertex elefant=katze.add(maus);
-		
-		System.out.println(katze);
+		Vertex v2=new Vertex(42,23);
+		Vertex v3=new Vertex(0,0);
+		Vertex v4=new Vertex(42,23);
 		
 		
-		katze.addMod(maus);
-		System.out.println(katze);
-		System.out.println(elefant);
+		System.out.println(v2.toString());
+		System.out.println(v2);
+		System.out.println(v1.length());
+		System.out.println(v2.length());
+		System.out.println(v3.length());
+		System.out.println(v1.skalarMult(0.8));
+		System.out.println(v1);
+		System.out.println(v1.add(v2));
+		System.out.println(v1);
+		System.out.println(v2);
+		
+		System.out.println(v1.equals(v2));
+		System.out.println(v2.equals(v3));
+		System.out.println(v2.equals(v4));
+		System.out.println(v4.equals(v2));
+		
+		
+		
 
 	}
 
