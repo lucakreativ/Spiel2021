81c0726fb01fc25761e8b7c4eb0f7d3a34a3ce6f
servver <servver@servver-ESPRIMO-Mobile-M9410>
2021-10-05 15:28:46 +0200

    geometric object constructor

---
+++ Spie_2021/src/GeometricObject.java
@@ -0,0 +1,60 @@
+public class GeometricObject {
+	public Vertex pos;
+	public double height;
+	public double width;
+
+	public GeometricObject(double width, double height, Vertex pos) {
+
+		this.pos = pos;
+		this.width = width;
+		this.height = height;
+
+		if (width < 0) {
+			width = -width;
+			pos.x = pos.x - width;
+		}
+
+		if (height < 0) {
+			height = -height;
+			pos.y = pos.y - width;
+		}
+
+	}
+
+	public GeometricObject(double width, double height) {
+		this(width, height, new Vertex(0, 0));
+	}
+
+	public GeometricObject(double w) {
+		this(w, w);
+	}
+	
+	public GeometricObject() {
+		this(10);
+	}
+	
+	
+	
+	
+	public String toString()
+	{
+		return "Width: "+width+", Height: "+height+", Pos: "+pos;
+	}
+	
+	public double circumference() {return 2*(width+height);}
+	
+	public double area() {return width*height;}
+	
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public Vertex getPos() {
+		return pos;
+	}
+}
