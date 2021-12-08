db31177264deac9221f1a2ed2250f68cb2a8bd8d
servver <servver@servver-ESPRIMO-Mobile-M9410>
2021-12-08 09:06:32 +0100

    romve, turn

---
+++ Spie_2021/src/AnimationPanel.java
@@ -0,0 +1,35 @@
+import util.ShowInFrame;
+
+public class AnimationPanel extends SimpleAnimation{
+	
+	@Override public void doOnTick() {
+		move();
+		doChecks();
+		repaint();
+	}
+	
+	public void doChecks() {
+		
+		for(GameObject g1:gos)
+			for(GameObject g2:gos)
+				if(g1!=g2&&g1.touches(g2))
+					reactWhenTouching(g1,g2);
+		
+	}
+	
+	void reactWhenTouching(GameObject g1, GameObject g2) {
+		g1.turn();
+		g1.move();
+		
+		g2.turn();
+		g2.move();
+	}
+
+	public static void main(String[] args) {
+		MovablePanel p=new AnimationPanel();
+		p.gos.add(new MovableImage("hexe.png",0,0,1,1));
+		p.gos.add(new MovableImage("biene.png",400,400,-1,-1));
+		ShowInFrame.show(p);
+	}
+
+}
--- Spie_2021/src/SimpleAnimation.java
+++ Spie_2021/src/SimpleAnimation.java
@@ -2,19 +2,21 @@ import javax.swing.*;
 import java.awt.event.*;
 import util.ShowInFrame;
 
-public class SimpleAnimation extends MovableImage{
-	
-	Timer t =new Timer(30, new ActionListener() {
+public class SimpleAnimation extends MovablePanel{
+
+	Timer t =new Timer(50, new ActionListener() {
 		public void actionPerformed(ActionEvent e) {doOnTick();}
 	});
 	
 	SimpleAnimation(){t.start();}
 	
+	
 	public static void main(String[] args) {
 		MovablePanel p = new SimpleAnimation();
-		p.mp.gos.add(new MovableImage("hexe.jpeg", 0,0,1,1));
-		p.mp.gos.add(new MovableImage("biene.jpeg", 400, 400, -1, -1));
+		p.gos.add(new MovableImage("hexe.png",0,0,1,1));
+		p.gos.add(new MovableImage("biene.png",400,400,-1,-1));
 		ShowInFrame.show(p);
+
 	}
-	
+
 }
